2d76f554fbd487e56ee82c97dbcd78da0edcc265
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-05-24 03:57:03 +0530

    Fixed window.location.hash issue

--- src/components/redirect.js
+++ src/components/redirect.js
@@ -7,9 +7,10 @@ import { withRouter } from 'react-router-dom'
 const Redirect = withRouter(({ history }) => {
   var [text, setText] = useState("");
   const continueFunction = () => {
-    const search = window.location.search;
+      console.log(window.location)
+    const search = window.location.hash;
     const params = new URLSearchParams(search);
-    const token = params.get("access_token");
+    const token = params.get("#access_token");
     if (token) {
       setText("Signed in successfully");
       localStorage.setItem('api-token', token)
