619512e80a5351dcd548dad9a96d9d80a4cef585
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-05-24 02:16:26 +0530

    updated redirectLink

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { Redirect } from 'react-router';
 
 const redirectConfig = {
   web_client_id: '348da7c2174c73ec9ed932f708d30b0c87df5ac99240326be55b93f939f31976',
-  redirect_uri: 'https://localhost:3000/redirect'
+  redirect_uri: 'https://gitlab-deployer.herokuapp.com/redirect'
 }
 const redirectLink = 'https://gitlab.com/oauth/authorize?client_id=' + redirectConfig.web_client_id + '&redirect_uri=' + redirectConfig.redirect_uri + '&response_type=token'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <button
           onClick={() => window.open(redirectLink)}
