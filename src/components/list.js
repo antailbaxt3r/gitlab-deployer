5029d023e5228cbdf8ba009f27cff30ad6eb5583
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-05-24 06:03:42 +0530

    Added projects list

--- src/components/list.js
+++ src/components/list.js
@@ -1,37 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 const axios = require("axios").default;
-axios.defaults.baseURL = "https://gitlab.com/api/v4";
+axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/https://gitlab.com/api/v4";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] =
+  "*";
 const token = localStorage.getItem("api-token");
 
 const ListScreen = () => {
-  var [username, setusername] = useState("0");
-  var [name, setname] = useState("0");
+  var [username, setusername] = useState("");
+  var [name, setname] = useState("");
+  var [projectsList, setProjectsList] = useState([]);
 
-  if (token) {
-    axios
-      .get("/user", {
-        params: {
-          access_token: token,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        setusername(response.data.username);
-        setname(response.data.name);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Could not fetch details");
-      });
-  } else {
-    alert("Not signed in!");
-  }
+  useEffect(() => {
+    const getUser = async () => {
+      await axios
+        .get("/user", {
+          params: {
+            access_token: token,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setusername(response.data.username);
+          setname(response.data.name);
+          const getList = async () => {
+            await axios
+              .get("users/" + response.data.username + "/projects", {
+                params: {
+                  access_token: token,
+                },
+              })
+              .then((projects) => {
+                console.log("projects :", projects);
+                setProjectsList(projects.data);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          };
+          getList();
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Could not fetch details");
+        });
+    };
+    getUser();
+  });
+
+
   return (
     <div className="ListScreen">
       <header className="App-header">
         <p>Name: {name}</p>
         <p>Username: {username}</p>
+        {projectsList.map((project) => (
+          <div key={project.id} style={{ background: "white", borderRadius: 10, width: "60%", padding: 10, margin: 10}}>
+            <p style={{color:'black'}}>{project.name}</p>
+            <p style={{color:'black'}}>{project.description}</p>
+          </div>
+        ))}
       </header>
     </div>
   );
